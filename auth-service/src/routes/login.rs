6b580c703622efff36f415f6d7f4d2216db677de
carloslopezandara <lopezandara.carlos@gmail.com>
2025-10-19 16:47:13 +0200

    [Sprint 3][Task 2.5, 2.6] feat: update login route to use auth cookie and upadte Docker & CI/CD

--- auth-service/src/routes/login.rs
+++ auth-service/src/routes/login.rs
@@ -1,32 +1,44 @@
 use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use axum_extra::extract::CookieJar;
 use serde::{Deserialize, Serialize};
-use uuid::Uuid;
-use crate::{app_state::AppState, domain::{AuthAPIError,Email, Password}};
+
+use crate::{
+    app_state::AppState, 
+    domain::{AuthAPIError,Email, Password},
+    utils::auth::generate_auth_cookie,
+};
 
 pub async fn login(
     State(state): State<AppState>,
+    jar: CookieJar, // New!
     Json(request): Json<LoginRequest>,
-) -> Result<impl IntoResponse, AuthAPIError>  { 
-    let email = Email::parse(request.email.clone())?;
-    let password = Password::parse(request.password.clone())?;
+) -> (CookieJar, Result<impl IntoResponse, AuthAPIError>) {
+    let email = match Email::parse(request.email.clone()) {
+        Err(error) => return (jar, Err(error)),
+        Ok(email) => email,
+    };
+    let password = match Password::parse(request.password.clone()){
+        Err(error) => return (jar, Err(error)),
+        Ok(password) => password,
+    };    
     let user_store = state.user_store.read().await;
+    
+    match user_store.validate_user(&email, &password).await {
+        Err(_) => return (jar, Err(AuthAPIError::IncorrectCredentials)),
+        Ok(_) => (),
+    };
 
-    match user_store.get_user(&email).await {
-        Ok(_) => {
-            match user_store.validate_user(&email, &password).await {
-                Ok(_) => {
-                    let response = Json(LoginResponse {
-                        message: "Login successful".to_string(),
-                        login_attemp_id: Uuid::new_v4().to_string(),
-                    });
-                    Ok((StatusCode::OK, response))
-                },
-                Err(_) => Err(AuthAPIError::IncorrectCredentials),
-            }
-        },
-        Err(_) => Err(AuthAPIError::IncorrectCredentials),
-    }
+    let _user = match user_store.get_user(&email).await {
+        Err(_) => return (jar, Err(AuthAPIError::IncorrectCredentials)),
+        Ok(user) => user,
+    };
 
+    let auth_cookie = match generate_auth_cookie(&email) {
+        Err(_) => return (jar, Err(AuthAPIError::UnexpectedError)),
+        Ok(cookie) => cookie,
+    };
+    let updated_jar = jar.add(auth_cookie);
+    (updated_jar, Ok(StatusCode::OK.into_response()))
 }
 
 #[derive(Deserialize)]
