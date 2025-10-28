58e3920d1a798f26894a553b427b7dcec3425f90
carloslopezandara <lopezandara.carlos@gmail.com>
2025-10-28 20:46:29 +0100

    [Sprint 4][Task 2.2, 2.3] feat: add EmailClient trait and MockEmailClient implementation for email handling

--- auth-service/src/domain/mod.rs
+++ auth-service/src/domain/mod.rs
@@ -3,9 +3,11 @@ mod error;
 mod data_stores;
 mod email;
 mod password;
+mod email_client;
 
 pub use user::*;
 pub use error::*;
 pub use data_stores::*;
 pub use email::*;
+pub use email_client::*;
 pub use password::*;
